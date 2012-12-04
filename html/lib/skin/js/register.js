1711ded983b8e97923e96a4eac9416f375204584
Jim A Kinsman <jakinsman@gmail.com>
2012-12-04 14:44:02 -0600

    better initial commit part 2

--- html/lib/skin/js/register.js
+++ html/lib/skin/js/register.js
@@ -1,22 +1,5 @@
 $(function(){
    if (document.frmRegister){
-        var orglogin = $(document.frmRegister.org_login);
-        var orgname = $(document.frmRegister.org_name);
-        orglogin.change(function(){
-           $(this).attr('noautomodify', true);
-        });
-        
-        var makeOrgShortname = function(){
-           if (orglogin.attr('noautomodify') == undefined){
-              var lcval = orgname.val().toLowerCase();
-              lcval = lcval.replace(/ |-/g,"_");
-              orglogin.val(lcval);
-           }
-        };
-        
-        orgname.on('keyup click', makeOrgShortname);
-        
-        
        //onsubmit
        $(document.frmRegister).submit(function(){
           if (!$('input[name=plan]:checked', this).val()){
@@ -25,6 +8,4 @@ $(function(){
           }
        });        
    }
-   
-
 });
